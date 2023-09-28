6281ea6c000fe9114018d09690eaeecf0058f80c
saurabhkothari <saurabhkothari@rocketmail.com>
2023-09-28 11:19:28 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ami_id" {
+  default = "ami-0c55b159cbfafe1f0"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "my-key-pair"
+}
+
+variable "instance_name" {
+  default = "example-instance"
+}

6f5d06fd83aeda3da63e320d1a61f8fdc991e08e
saurabhkothari <saurabhkothari@rocketmail.com>
2023-10-26 12:58:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
+
+  tags = {
+    Name = var.instance_name  # Custom name for the EC2 instance
+  }
 }
 
 output "instance_ip" {
